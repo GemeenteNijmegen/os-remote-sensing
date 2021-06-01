bcaeccc1c0878a7f58dda699b60497261315fa56
S H <hoorns@apeldoorn.nl>
2021-06-01 06:45:45 +0000

    Replace start.py

--- Processing/start.py
+++ Processing/start.py
@@ -1,8 +1,8 @@
 # Load required Python libraries
 
 
-# Vairabelen
-workingdirectory = "J:\DatalAP\Innovatie\VNG_Remote_Sensing\GroeneDaken\Werk"
-sourcedirectoryECW = r"\\apeldoorn.nl\base$\groupdata\DatalAP\Bronnen\beeldmateriaal\25cm\2020"
+# Variabelen
+workingdirectory = "\\Werk"
+sourcedirectoryECW = r"\\Bronnen\beeldmateriaal\25cm\2020"
 
 buurtcode = "BU03638600"
