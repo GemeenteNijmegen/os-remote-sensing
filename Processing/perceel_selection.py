1510da5268aefd38eb79a4f0704eda16e59517ba
S H <hoorns@apeldoorn.nl>
2021-07-13 09:23:28 +0000

    Replace perceel_selection.py

--- Processing/perceel_selection.py
+++ Processing/perceel_selection.py
@@ -1,48 +1,42 @@
 #Load required Python libraries
+import pandas as pd
 import geopandas as gpd
 from requests import Request
 from owslib.wfs import WebFeatureService
 
 # Load variables
-from variables import *
+from start import gpkg_vector
 
 ## Start script
 # Read buurt
 gdf_buurt = gpd.read_file(gpkg_vector, driver='GPKG', layer='buurt')
 
-#BoundingBox
-bbox_geom = gdf_buurt.bounds
+loops = [0,1000,2000,3000,4000,5000]
 
-lowerCorner = bbox_geom['minx'].astype(str) + ' ' + bbox_geom['miny'].astype(str)
-upperCorner = bbox_geom['maxx'].astype(str) + ' ' + bbox_geom['maxy'].astype(str)
+empty_df = []  # empty list which will hold your dataframes
 
-# URL for WFS backend
-url_WFSpercelen = "https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?"
+for loop in loops:
+    bbox_geom = gdf_buurt.bounds
+    lowerCorner = bbox_geom['minx'].astype(str) + ' ' + bbox_geom['miny'].astype(str)
+    upperCorner = bbox_geom['maxx'].astype(str) + ' ' + bbox_geom['maxy'].astype(str)
+    url_WFSpercelen = "https://geodata.nationaalgeoregister.nl/kadastralekaart/wfs/v4_0?"
+    wfs = WebFeatureService(url=url_WFSpercelen)
+    layer = 'kadastralekaartv4:perceel'
+    filter = ('<ogc:Filter>'
+                '<ogc:BBOX>'
+                    '<ogc:PropertyName>geometry</ogc:PropertyName>'
+                        '<gml:Envelope srsName="http://www.opengis.net/gml/srs/epsg.xml#28992"><gml:lowerCorner>' + lowerCorner + '</gml:lowerCorner><gml:upperCorner>' + upperCorner + '</gml:upperCorner></gml:Envelope>'
+                '</ogc:BBOX>'
+            '</ogc:Filter>')
+    startindex = loop
+    params = dict(service='WFS', version="2.0.0", request='GetFeature', typeName=layer, filter=filter, startindex=startindex, sortBy='identificatieLokaalID', outputFormat='json')
+    q = Request('GET', url_WFSpercelen, params=params).prepare().url
+    gdf_percelenbuurt = gpd.read_file(q)
+    empty_df.append(gdf_percelenbuurt)
 
-# Initialize
-wfs = WebFeatureService(url=url_WFSpercelen)
+gdf_allpercelenbuurt = pd.concat(empty_df, ignore_index=True)
 
-# Select layer
-layer = 'kadastralekaartv4:perceel'
+gdf_allpercelenbuurt = gdf_allpercelenbuurt.set_crs("EPSG:28992")
+gdf_allpercelenbuurt = gpd.clip(gdf_allpercelenbuurt, gdf_buurt)
 
-filter = ('<ogc:Filter>'
-            '<ogc:BBOX>'
-                '<ogc:PropertyName>geometry</ogc:PropertyName>'
-                    '<gml:Envelope srsName="http://www.opengis.net/gml/srs/epsg.xml#28992"><gml:lowerCorner>' + lowerCorner + '</gml:lowerCorner><gml:upperCorner>' + upperCorner + '</gml:upperCorner></gml:Envelope>'
-            '</ogc:BBOX>'
-        '</ogc:Filter>')
-
-
-# Specify the parameters for fetching the data
-params = dict(service='WFS', version="2.0.0", request='GetFeature', typeName=layer, filter=filter, outputFormat='json')
-
-# Parse the URL with parameters
-q = Request('GET', url_WFSpercelen, params=params).prepare().url
-
-# Read data from URL
-gdf_percelenbuurt = gpd.read_file(q)
-
-gdf_percelenbuurt = gdf_percelenbuurt.set_crs("EPSG:28992")
-gdf_percelenbuurt = gpd.clip(gdf_percelenbuurt, gdf_buurt)
-
-gdf_percelenbuurt.to_file(gpkg_vector, driver='GPKG', layer='percelen')
+gdf_allpercelenbuurt.to_file(gpkg_vector, driver='GPKG', layer='percelen')
