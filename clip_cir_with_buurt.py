fd6d9f38f9c12526656a4a7dfd3cbcb2b3c2fba9
S H <hoorns@apeldoorn.nl>
2021-07-16 13:50:40 +0000

    Upload New File

---
+++ clip_cir_with_buurt.py
@@ -0,0 +1,51 @@
+# Load required Python libraries
+import geopandas as gpd
+
+# Load variables
+from start import gpkg_vector, path_ECW_2020, files_basename, gpkg_raster
+
+
+# Read buurt
+gdf_buurt = gpd.read_file(gpkg_vector, driver='GPKG', layer='buurt')
+
+# Lijkt alleen op deze volgorde te werken. Liever allee libraries in één keer inladen, maar dat werkt niet goed
+
+#Rest van libraries inladen
+from osgeo import gdal
+import rasterio
+from rasterio.mask import mask
+from rasterio.plot import show
+import fiona
+import rasterio.mask
+
+def getFeatures(gdf):
+    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
+    import json
+    return [json.loads(gdf.to_json())['features'][0]['geometry']]
+
+#Read buurt polygoon
+coordsbuurt = getFeatures(gdf_buurt)
+
+#Clip Luchtfoto with Buurt from Geopackage
+#Source: https://automating-gis-processes.github.io/CSC18/lessons/L6/clipping-raster.html
+with rasterio.open(path_ECW_2020) as src:
+    out_image, out_transform = rasterio.mask.mask(src, coordsbuurt, crop=True)
+    out_meta = src.meta
+
+out_meta.update({"driver": "GTiff",
+                 "height": out_image.shape[1],
+                 "width": out_image.shape[2],
+                 "transform": out_transform})
+
+#Write clipped tif
+with rasterio.open(files_basename + ".tif", "w", **out_meta) as dest:
+    dest.write(out_image)
+
+#Write to GPKG
+import os
+desGPKG = gpkg_raster
+sourcetif_cir = files_basename + ".tif"
+
+lyr_cir = "cir"
+gdal_string_cir = 'gdal_translate -of GPKG "{}" "{}" -co RASTER_TABLE={} -co APPEND_SUBDATASET=YES'.format(sourcetif_cir, desGPKG, lyr_cir)
+os.system(gdal_string_cir)
