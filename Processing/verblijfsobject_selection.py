05a16bea2660e1612a6cbf7995cabb5076d688a0
S H <hoorns@apeldoorn.nl>
2021-09-08 12:43:44 +0000

    Replace verblijfsobject_selection.py

--- Processing/verblijfsobject_selection.py
+++ Processing/verblijfsobject_selection.py
@@ -7,14 +7,17 @@ from owslib.wfs import WebFeatureService
 # Load variables
 from start import gpkg_vector
 
-## Start script
-# Read buurt
+## Start verblijfsobject selection script
+# Read buurt-polygon
 gdf_buurt = gpd.read_file(gpkg_vector, driver='GPKG', layer='buurt')
 
+# Define startindex for WFS-request. A maximum of 1000 features return from WFS-request. To receive all features in a buurt the startindex for each request is new.
 loops = [0,1000,2000,3000,4000,5000]
 
+# Create empty dataframe
 empty_df = []  # empty list which will hold your dataframes
 
+# Multiple WFS-requests
 for loop in loops:
     bbox_geom = gdf_buurt.bounds
     lowerCorner = bbox_geom['minx'].astype(str) + ' ' + bbox_geom['miny'].astype(str)
@@ -31,25 +34,26 @@ for loop in loops:
     startindex = loop
     params = dict(service='WFS', version="2.0.0", request='GetFeature', typeName=layer, filter=filter, startindex=startindex, sortBy='identificatie', outputFormat='json')
     q = Request('GET', url_WFSbagvo, params=params).prepare().url
-    gdf_vobuurt = gpd.read_file(q) #Deze runt mogelijk niet op inno pc 21931
+    gdf_vobuurt = gpd.read_file(q)
     empty_df.append(gdf_vobuurt)
 
+# Merge all WFS-rquest in one df
 gdf_allvobuurt = pd.concat(empty_df, ignore_index=True)
 
+# Set projection
 gdf_allvobuurt = gdf_allvobuurt.set_crs("EPSG:28992")
 
+# Clip verblijfsobjecten with buurt
 gdf_allvobuurt = gpd.clip(gdf_allvobuurt, gdf_buurt)
 
+# Write verblijfsobjecten to gpkg
 gdf_allvobuurt.to_file(gpkg_vector, driver='GPKG', layer='verblijfsobjecten')
 
-#Selecteren alle verblijfsobjecten met de status 'in gebruik' en 'verbouwing'
+# Filter all verblijfsobjecten with status 'in gebruik' and 'verbouwing'
 gdf_vobuurtwoonfunctie = gdf_allvobuurt[gdf_allvobuurt['status'].str.contains('Verblijfsobject in gebruik|Verbouwing verblijfsobject')]
 
-#Selecteren alle verblijfsobjecten waar 'woonfunctie' en 'logiesfunctie' in voorkomt
+# Filter all verblijfsobjecten which contain the function 'woonfunctie' and 'logiesfunctie'
 gdf_vobuurtwoonfunctie = gdf_allvobuurt[gdf_allvobuurt['gebruiksdoel'].str.contains('woonfunctie|logiesfunctie')]
 
-#Selecteren alle verblijfsobjecten met de status 'in gebruik'
-gdf_vobuurtwoonfunctie = gdf_vobuurtwoonfunctie[gdf_vobuurtwoonfunctie['status'].str.contains('Verblijfsobject in gebruik|Verbouwing verblijfsobject')]
-
-#Alle verblijfsobjecten opslaan die 'Woonfunctie' bevatten in het gebruiksdoel
+# Write woningen to gpkg
 gdf_vobuurtwoonfunctie.to_file(gpkg_vector, driver='GPKG', layer='woningen')
