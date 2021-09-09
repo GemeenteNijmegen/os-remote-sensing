10d608b01e17bd84f8e088783d63bc7f1748298d
S H <hoorns@apeldoorn.nl>
2021-09-09 09:54:00 +0000

    Upload New File

---
+++ Processing/garden_vegetation_cover_stats.py
@@ -0,0 +1,54 @@
+#Import libraries
+import os
+from rasterstats import zonal_stats
+import pandas as pd
+import geopandas as gpd
+
+from start import files_basename, gpkg_vector
+
+#Source of code
+#https://gis.stackexchange.com/questions/408583/zonal-histogram-python
+
+"""
+Hieronder wordt het percentage groen per tuin berekend.
+"""
+#Input voor percentage groen berekening
+raster = files_basename + "_tuinen_ndvi_3_classes.tif"
+#zones = files_basename + "_tuinen_polygon.shp"
+
+#Berekening percentage groen dmv dataframe
+df = gpd.read_file(gpkg_vector, driver='GPKG', layer='tuinen')
+zs = zonal_stats(vectors=df['geometry'], raster=raster, categorical=True)
+dfz = pd.DataFrame(zs).fillna(0)
+
+dfz_perc = dfz.apply(lambda x: round(x/x.sum()*100,1), axis=1) #From pixel count to percentage
+dfres = pd.merge(left=df, right=dfz_perc, how='left', left_index=True, right_index=True)
+
+#Hernoemen kolom '10'
+#'10' = perc_groen (percentage groen)
+# '20' = overige percentage (totaal - percentage groen)
+
+#Change column data types to string for all columns
+dfres.columns = dfres.columns.astype(str)
+
+#Get data type for each column
+print(dfres.columns.map(type))
+
+#delete column, this column contains 100% - percentage vegetation
+del dfres['20']
+
+dfres.rename(columns={'10': 'pGROEN'}, inplace=True)
+
+#Afronden kolommen naar 0 decimalen
+dfres['pGROEN'] = dfres['pGROEN'].astype(float)
+dfres['pGROEN'] = dfres['pGROEN'] + 0.5
+dfres['pGROEN'] = dfres['pGROEN'].astype(int)
+dfres['perceelnummer'] = dfres['perceelnummer'].astype(int)
+
+#Get column names dataframe
+print(dfres.columns.tolist())
+
+#Wegschrijven naar shapefile
+#dfres.to_file(zones)
+
+dfres.to_file(gpkg_vector, driver='GPKG', layer='tuinen_stats')
