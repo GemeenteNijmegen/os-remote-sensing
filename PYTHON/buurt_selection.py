42323d58e90d0e887048f8824eec09a69edc27bc
Hans <hans.weda@rond.nu>
2022-03-01 14:10:14 +0100

    A function get_buurtgrens has been added. This function retrieves only a particular buurtgrens instead of downloading all buurtgrenzen for the whole of the Netherlands. This significantly increases the processing speed.

--- PYTHON/buurt_selection.py
+++ PYTHON/buurt_selection.py
@@ -1,35 +1,13 @@
 # Load required Python libraries
-import geopandas as gpd
-from requests import Request
-from owslib.wfs import WebFeatureService
+from utils.area_boundaries import get_buurtgrens
 
 # Load variables
 from start import buurtcode, gpkg_vector
 
 ## Start buurt selection script
-# URL for WFS backend
-url_WFSwijkenbuurt = "https://geodata.nationaalgeoregister.nl/wijkenbuurten2020/wfs?"
-
-# Initialize
-wfs = WebFeatureService(url=url_WFSwijkenbuurt)
-
-# Select layer
-layer = 'wijkenbuurten2020:cbs_buurten_2020'
-
-# Specify the parameters for fetching the data
-params = dict(service='WFS', version="1.0.0", request='GetFeature', typeName=layer, outputFormat='json')
-
-# Parse the URL with parameters
-q = Request('GET', url_WFSwijkenbuurt, params=params).prepare().url
-
-# Read data from URL
-gdf_buurten = gpd.read_file(q)
 
 # Select columns
-gdf_buurten = gdf_buurten.loc[:, ['buurtcode', 'buurtnaam', 'gemeentecode', 'geometry']]
-
-# Filter buurt from csv with buurtcodes
-gdf_buurtselection = gdf_buurten[gdf_buurten.buurtcode == buurtcode]
+gdf_buurtselection = get_buurtgrens(buurtcode=buurtcode, peiljaar=2020)
 
 # Set projection
 gdf_buurtselection = gdf_buurtselection.set_crs("EPSG:28992")
